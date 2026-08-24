8a34f2db6a5f03745b3708791f6bf8abb27ec35a
Jassy <73526153+JassyEpic@users.noreply.github.com>
2026-08-24 18:43:29 +0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("FRONTEND_URL", "http://mobilheim-union.de"),
+  url: env("FRONTEND_URL", "http://api.mobilheim-union.de"),
   app: {
     keys: env.array("APP_KEYS"),
   },
